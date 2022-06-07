27f9bf110c74c77c50be3cc2be44fc894f90a461
Faisal Ramadiansyah <faisal@fxmweb.com>
2022-06-07 23:02:22 +0700

    update: 1st week exercise and documentation

---
+++ week-1/README.md
@@ -0,0 +1,62 @@
+# Week 1
+
+## Setup
+
+### Prerequisites
+
+1. [Visual Studio Code/VSCode](https://code.visualstudio.com/)
+1. [REST Client VSCode extension](https://marketplace.visualstudio.com/items?itemName=humao.rest-client) (to test API)
+1. [Node.js LTS version](https://nodejs.org/en/download/)
+1. Your favorite command prompt/CMD/Bash/Shell/Terminal
+
+
+### First time install
+
+Navigate your command prompt to `/week-1` directory, and type in:
+
+```shell
+npm install
+```
+
+### Running the app
+
+Navigate your command prompt to `/week-1` directory, and type in:
+
+```shell
+node main.js
+```
+
+## Meeting Agenda and Notes
+
+### Meeting Recording
+
+[You can view the recording of the meeting here](https://staging.fxmediawebapp.com/training/react-and-node/study-node-and-react--week-1.mp4).
+
+### Objectives
+
+1. How to setup Node - https://nodejs.org/en/download/
+1. How to setup MongoDB - https://www.mongodb.com/try/download/community
+1. How to develop API CRUD with Node
+1. GET − This is used to provide a read only access to a resource.
+1. PUT/PATCH − This is used to update a resource.
+1. DELETE − This is used to remove a resource.
+1. POST − This is used to update a existing resource or create a new resource.
+1. How to test API
+
+### What is Node.js?
+
+The simplest definition of Node.js is that it is a Javascript run-time environment that helps in the execution of JavaScript code server-side. It is an open–source cross-platform JavaScript that helps in the development of real-time network application.
+
+#### Pros of Node.js
+1. Node.js offers an Easy Scalability
+1. Easy to Learn
+1. Node.js is used as a Single Programming Language
+1. The Benefit of Fullstack JS
+1. Known for Offering High Performance
+1. The Support of Large and Active Community
+1. The Advantage of Caching
+1. Node.js is Highly Extensible
+
+### Meeting Notes
+
+1. When download Node.js version, there are two types of version: LTS and Current. Please download (it's recommended) the LTS version. LTS version known for its stability and support. If we want to download the "Current" version, make sure we know the consequences as it is might not stable to use on production environment.
---
+++ week-1/main.js
@@ -0,0 +1,166 @@
+const SERVER_PORT = 8020;
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const usersFilePath = path.resolve(__dirname, 'users.json');
+
+app.use(bodyParser.json({ limit: '1000kb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '1000kb' }));
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  fs.readFile(usersFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.json({
+        result: 'ERROR',
+        message: err.message
+      });
+    }
+
+    const users = JSON.parse(data);
+
+    res.json({
+      result: 'OK',
+      totalCount: Object.keys(users).length,
+      data: users
+    });
+  });
+});
+
+app.get('/users/:userId', (req, res) => {
+  fs.readFile(usersFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.json({
+        result: 'ERROR',
+        message: err.message
+      });
+    }
+
+    const paramUserId = req.params.userId;
+    const users = JSON.parse(data);
+    const selectedUser = users[paramUserId]
+
+    if (selectedUser) {
+      res.json({
+        result: 'OK',
+        data: selectedUser
+      });
+    } else {
+      res.json({
+        result: 'ERROR',
+        message: `User with ID ${paramUserId} does not exist.`
+      });
+    }
+  });
+});
+
+app.delete('/users/:userId', (req, res) => {
+  fs.readFile(usersFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.json({
+        result: 'ERROR',
+        data: err.message
+      });
+    }
+
+    const paramUserId = req.params.userId;
+    const users = JSON.parse(data);
+    const selectedUser = users[paramUserId];
+
+    if (selectedUser) {
+      delete users[paramUserId];
+
+      res.json({
+        result: 'OK',
+        message: `User with ID ${paramUserId} deleted.`,
+        data: users
+      });
+    } else {
+      res.json({
+        result: 'ERROR',
+        message: `User with ID ${paramUserId} does not exist.`
+      });
+    }
+  });
+});
+
+app.post('/user/add', (req, res) => {
+  fs.readFile(usersFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.json({
+        result: 'ERROR',
+        data: err.message
+      });
+    }
+
+    const users = JSON.parse(data);
+    const bodyData = req.body;
+    const id = bodyData.id;
+    const name = bodyData.name;
+    
+    if (users[id]) {
+      res.json({
+        result: 'ERROR',
+        message: `User with ID ${id} already exist. Please use different ID.`
+      });
+    } else {
+      users[id] = { name };
+      
+      res.json({
+        result: 'OK',
+        message: `User (${name}) added.`,
+        data: users
+      });
+    }
+  });
+});
+
+app.put('/user/:userId', (req, res) => {
+  fs.readFile(usersFilePath, 'utf8', (err, data) => {
+    if (err) {
+      res.json({
+        result: 'ERROR',
+        data: err.message
+      });
+    }
+
+    const paramUserId = req.params.userId;
+    const users = JSON.parse(data);
+    const selectedUser = users[paramUserId];
+    const bodyData = req.body;
+    const beforeUpdate = JSON.stringify(selectedUser);
+    const updated = {
+      name: bodyData.name
+    };
+
+    if (selectedUser) {
+      Object.assign(selectedUser, updated);
+
+      const afterUpdate = JSON.stringify(selectedUser);
+
+      res.json({
+        result: 'OK',
+        message: `User with ID ${paramUserId} updated.`,
+        before: beforeUpdate,
+        after: afterUpdate,
+        data: users
+      });
+    } else {
+      res.json({
+        result: 'ERROR',
+        message: `User with ID ${paramUserId} does not exist.`
+      });
+    }
+  });
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`App listening on localhost:${SERVER_PORT}`);
+});
